07feb8907f8b6b5c38c135c27778fdafbdfc2407
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-08-01 19:42:58 +0530

    app init

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import "./globals.css"
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+import { Metadata } from "next"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Google Docs",
+  description: "Google Docs Clone",
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
