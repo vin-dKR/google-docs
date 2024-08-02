573067770b3e2b40a3ed5a68c6e5e0ca1c82795a
vin-dKR <vinodkumarmurmu62@gmail.com>
2024-08-02 20:57:03 +0530

    navbar init ready

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/ui/Header'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className='flex w-fit items-center justify-center gap-2'>
+          <p className='text-white'>Document</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   )
